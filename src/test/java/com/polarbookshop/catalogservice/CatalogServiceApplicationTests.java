30ff14efafc6d4be85797d81aad2d06d898283f6
Hwang <dohauzi@gmail.com>
2024-07-14 21:15:49 +0900

    DB 테스트 추가

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -4,12 +4,14 @@ import com.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(	// 완전한 스프링 웹 애플리케이션 콘텍스트와 임의의 포트를 듣는 서블릿 컨테이너를 로드한다.
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@ActiveProfiles("integration")	// application-integration.yml 에서 설정을 로드하기 위해 integration 프로파일을 활성화한다.
 class CatalogServiceApplicationTests {
 
 	@Autowired	// 생성자 기반 의존성 주입이 권장되나 테스트 클래스에서는 이 방식이 허용된다.
