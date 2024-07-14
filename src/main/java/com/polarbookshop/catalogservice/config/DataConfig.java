5d72b0a4e03405af4aab5a29f7e7b1d6ec9c13cf
Hwang <dohauzi@gmail.com>
2024-07-14 10:33:33 +0900

    주석 추가

--- src/main/java/com/polarbookshop/catalogservice/config/DataConfig.java
+++ src/main/java/com/polarbookshop/catalogservice/config/DataConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 
 // 데이터의 생성, 변경, 삭제가 일어날 때마다 감사 이벤트가 생성된다.
-@Configuration
+@Configuration      // 이 클래스가 스프링 설정의 소스임을 나타낸다.
 @EnableJdbcAuditing // 지속성 엔티티에 대한 감사를 활성화(언제수정했고 누가수정했고 체크)
 public class DataConfig {}
