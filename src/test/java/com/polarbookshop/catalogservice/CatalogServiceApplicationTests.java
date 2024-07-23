daff908974e2d1f6c9262a4fe44e243a8e48666e
Hwang <dohauzi@gmail.com>
2024-07-23 21:23:00 +0900

    변경 없음

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -3,17 +3,14 @@ package com.polarbookshop.catalogservice;
 import com.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(	// 완전한 스프링 웹 애플리케이션 콘텍스트와 임의의 포트를 듣는 서블릿 컨테이너를 로드한다.
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
-@AutoConfigureTestDatabase( // 테스트컨테이너를 이용해야 하기 때문에 내장 테스트 데이터베이스 사용을 비활성화한다.
-		replace = AutoConfigureTestDatabase.Replace.NONE
 )
 @ActiveProfiles("integration")	// application-integration.yml 에서 설정을 로드하기 위해 integration 프로파일을 활성화한다.
 class CatalogServiceApplicationTests {
