e7492681799f0e6ac53a728d724c773a0c76e26c
dohauzi@gmail.com <dohauzi@gmail.com>
2024-11-30 18:34:02 +0900

    로그 ㅊ추가

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -1,15 +1,21 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@Slf
 @SpringBootApplication
 @ConfigurationPropertiesScan	// 스프링 콘텍스트에 설정 데이터 빈을 로드한다.
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CatalogServiceApplication.class, args);
+		var ctx = SpringApplication.run(CatalogServiceApplication.class, args);
+
+		// Bean이 제대로 로드되었는지 확인
+		var properties = ctx.getBean(PolarProperties.class);
+		log.info("Main: ClientProperties loaded = {}", properties);
 	}
 
 }
